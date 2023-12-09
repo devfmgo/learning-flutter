bb83ea74e23c8113cf2df2962ed7d61f3c4e3a85
devfm <devfm.go@gmail.com>
2023-12-10 06:08:59 +0700

    Learn Data Structure,List,Map in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,12 +14,54 @@ class MyApp extends StatelessWidget {
   double pi = 3.14159;
   bool isBeginner = true;
 
+  // Basic Function
+  void greet() {
+    print("Hello fik");
+  }
+
+  // function with paramters
+  void greetPerson(String name) {
+    print("Hello, " + name);
+  }
+
+  // function with return type
+  int add(int a, int b) {
+    int sum = a + b;
+    return sum;
+  }
+
+  // DATA STRUCTURE
+  // LIST
+  List<int> numbers = [1, 2, 3, 5];
+  List<String> names = ["Fikri", "Gita"];
+  void printNumbers() {
+    for (int i = 0; i < numbers.length; i++) {
+      print(numbers[i]);
+    }
+  }
+
+  void printName() {
+    for (int i = 0; i < names.length; i++) {
+      print(names[i]);
+    }
+  }
+
+  // SET : Underordered collection of unique elements
+  Set<String> uniqueNames = {"Fikri", "Gita"};
+
+  // MAP : store as key-value pairs
+  Map user = {'name': 'fikri', 'age': 29};
+
   @override
   Widget build(BuildContext context) {
-    // For Loop
-    for (var i = 0; i <= 5; i++) {
-      print(i);
-    }
+    /*
+      int mySum = add(3, 5);
+      print(mySum);
+      greetPerson("Fikri");
+      printNumbers();
+      printName();
+    */
+    print(user['name']);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
