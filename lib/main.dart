240d7e413cd770ac6c4659ed3abfdd0fa4f03498
devfm <devfm.go@gmail.com>
2023-12-10 21:45:20 +0700

    GridView with Stack

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // Sampel GridView
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.deepPurple,
-            margin: EdgeInsets.all(2),
-          ),
+        // Sampel GridView with Stack
+        body: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            // big box
+            Container(height: 300, width: 300, color: Colors.deepPurple[400]),
+            //medium box
+            Container(height: 200, width: 200, color: Colors.deepPurple[200]),
+            //small box
+            Container(height: 100, width: 100, color: Colors.deepPurple[100]),
+          ],
         ),
       ),
     );
